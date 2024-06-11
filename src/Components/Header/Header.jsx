7625f6bfb8209159ea17da08ba50a4b55956694c
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-06-11 17:50:20 +0530

    Header completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import "./Header.css";
+import OlxLogo from "../../assets/OlxLogo";
+import Search from "../../assets/Search";
+import Arrow from "../../assets/Arrow";
+import SellButton from "../../assets/SellButton";
+import SellButtonPlus from "../../assets/SellButtonPlus";
 
 const Header = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="headerParent">
+      <div className="headerChildDiv">
+        <div className="brandName">
+          <OlxLogo />
+        </div>
+        <div className="placeSearch">
+          <Search />
+          <input type="text" />
+          <Arrow />
+        </div>
+        <div className="productSearch">
+          <div className="input">
+            <input
+              type="text"
+              placeholder="Find car,mobile phone and more..."
+            />
+          </div>
+          <div className="searchAction">
+            <Search color="#ffffff" />
+          </div>
+        </div>
+
+        <div className="language">
+          <span>ENGLISH</span>
+          <Arrow />
+        </div>
+
+        <div className="loginPage">
+          <span>Login</span>
+          <hr />
+        </div>
+
+        <div className="sellMenu">
+          <SellButton />
+          <div className="sellMenuCotent">
+            <SellButtonPlus />
+            <span>SELL</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
