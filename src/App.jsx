e543c3e9a77c582c3bf2aaf5570fd95a8facebbc
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-06-11 16:48:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Home/>
+      </div>
+    </>
+  );
+}
+
+export default App;
