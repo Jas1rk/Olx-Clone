d69f6f149cee038272aebb9bfa9bf6ebb54e3785
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-06-18 18:45:22 +0530

    login

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import fireBaseContext from "./Storage/FirebaseContext.jsx";
-import firebase  from "./Firebase/Firebase.jsx";
+import firebase from "./Firebase/Firebase.jsx";
+import { UserContextProvider } from "./Storage/userContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <fireBaseContext.Provider value={{firebaseapp:firebase}}>
-    <App />
+  <fireBaseContext.Provider value={{ firebaseapp: firebase }}>
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </fireBaseContext.Provider>
 );
