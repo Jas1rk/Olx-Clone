a5c78e1a9e5314f211e646a5d91b565cef679818
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-06-24 22:25:24 +0530

    create

--- src/Hooks/CreateForm.jsx
+++ src/Hooks/CreateForm.jsx
@@ -2,16 +2,15 @@ import React, { useState } from "react";
 
 const CreateForm = (initalValue) => {
   const [values, setValues] = useState(initalValue);
-  return [
-    values,
-    (event) => {
-      const { name, type, files, value: inputValues } = event.target;
-      setValues((prevVal) => ({
-        ...prevVal,
-        [name]: type === "file" ? files[0] : inputValues,
-      }));
-    },
-  ];
+  const handleChange = (event) => {
+    const { name, type, files, value } = event.target;
+
+    setValues((prevVal) => ({
+      ...prevVal,
+      [name]: type === "file" ? files[0] : value,
+    }));
+  };
+  return [values, handleChange];
 };
 
 export default CreateForm;
